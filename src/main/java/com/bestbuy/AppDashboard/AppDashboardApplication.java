e31b4091f86969c65587c0c18630d0dd0c88244f
Likhith-3399 <likhith3399@gmail.com>
2018-01-29 10:01:52 -0600

    Initial commit

---
+++ src/main/java/com/bestbuy/AppDashboard/AppDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.bestbuy.AppDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppDashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bestbuy/AppDashboard/AppDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bestbuy.AppDashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AppDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
